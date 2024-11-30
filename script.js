60bc3e1b46e0371977616f6ab7ebdf94ec998a5e
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-11-30 23:50:51 +0530

    added transaction list features

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let add = document.querySelector(".add");
 let popUpSection = document.querySelector(".pop-up-section");
 let select = document.querySelector("#select");
+let category = document.querySelector("#category")
 let transName = document.querySelector(".trans-name")
 let transAmount = document.querySelector(".trans-amount")
 let transDate = document.querySelector(".trans-date")
@@ -19,7 +20,7 @@ document.addEventListener("click" , (event) =>{
  }
 
  if(event.target.classList.contains("submit")){
-    getData(transDate.value, select.value,  transName.value, transAmount.value)
+    getData(transDate.value, select.value,  transName.value, transAmount.value, category.value)
     transDate.value = "";
     select.value = "";
     transName.value = "" ;
@@ -32,22 +33,34 @@ let arr = [];
 let balanceAmount = 0;
 
 class transaction{
-   constructor (date , month , year , transType ,transName, transAmount ){
+   constructor (date , month , year , transType ,transName, transAmount,category ){
        this.date = date;
        this.month = month + 1;
        this.year = year;
        this.type = transType;
        this.amount = transAmount
        this.name = transName
+       this.category = category;
    }
 }
 
-function getData(date, transType , name , amount){
-   let newData = new transaction(new Date(date).getDate() ,new Date(date).getMonth(),new Date(date).getFullYear(),transType, name , amount);
+function getData(date, transType , name , amount,category){
+   let newData = new transaction(new Date(date).getDate() ,new Date(date).getMonth(),new Date(date).getFullYear(),transType, name , amount,category);
    console.log(newData.name);
+   displayOnUI(newData)
    arr.push(newData);
-
-   console.log(arr);
+//    let transactionListArr = document.querySelectorAll(".transactions-list");
+//    console.log(transactionListArr)
+   
+//      transactionListArr.forEach((value , index) =>{
+//          value.addEventListener("click" , (e) =>{
+//             //  console.log(e.target);
+//              if((e.target.classList.contains("del")) || (e.target.classList.contains("view"))){
+//                  console.log(index);
+//              }
+//          })
+//      })
+//    console.log(arr);
 
    if(transType == "income"){
     balanceAmount += Number(amount);
@@ -59,31 +72,58 @@ function getData(date, transType , name , amount){
     balance.innerText = `${balanceAmount}`;
    }
   
-   displayOnUI(arr);
+//    displayOnUI(arr);
 
 }
 
+let id = 0;
+function displayOnUI(value){
+   
+    // console.log(value.name);
 
-function displayOnUI(arr){
-    arr.forEach((value) =>{
-        console.log(value.name);
-        let transactionList = document.createElement("div");
-        let finalAmount = null;
+    let finalAmount = null;
 
-        if(value.type == "expense"){
-            finalAmount = `- ${value.amount}`
-        }else{
-            finalAmount = `+ ${value.amount}`
-        }
-        transactionList.add.classList("transactions-list");
-        transactionList.innerHTML = `
-         <div>
-             <button class="view">></button>
-             <p>${finalAmount}</p>
-         </div>
-         <p>-Rs. 140.00</p>
-        `
-    });
+    if(value.type == "expense"){
+        finalAmount = `- Rs. ${value.amount}`
+    }else{
+        finalAmount = `+ Rs. ${value.amount}`
+    }
 
-    console.log(transactionList);
+    let transactionList = document.createElement("div");
+    transactionList.className = "transactions-list";
+    transactionList.id = `${id++}`;
+    transactionList.innerHTML = `
+     <div>
+         <button class="view">
+        x
+         </button>
+         <p>${value.name}</p>
+     </div>
+     <p class="final-color">${finalAmount}</p>
+    `
+    transactionDetails.append(transactionList);
+    let finalColor = transactionList.querySelector(".final-color");
+    // console.log(finalColor)
+    if(value.type == "income"){
+        finalColor.style.backgroundColor = "green";
+    }else{
+        finalColor.style.backgroundColor = "red";
+
+    }
+
+  transactionList.addEventListener("click" , (e) =>{
+    if( e.target.classList.contains("view")){
+        console.log(e.target.parentNode.parentNode.id);
+        let delId = e.target.parentNode.parentNode.id;
+        
+        arr.splice(delId, 1);
+        console.log(arr);
+         e.target.parentNode.parentNode.remove();
+    }
+  })
+    
+
+
+   
 }
+
